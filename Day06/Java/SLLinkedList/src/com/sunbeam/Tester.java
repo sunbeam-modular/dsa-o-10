119367fbcf90b65d06da992ebc8ede264096bbc2
devendra.dhande <devendra.dhande@sunbeaminfo.com>
2025-02-01 20:24:13 +0530

    Day06

---
+++ Day06/Java/SLLinkedList/src/com/sunbeam/Tester.java
@@ -0,0 +1,25 @@
+package com.sunbeam;
+
+public class Tester {
+
+	public static void main(String[] args) {
+		
+		LinkedList list = new LinkedList();
+		
+		list.addFirst(40);
+		list.addFirst(30);
+		list.addFirst(20);
+		list.addFirst(10);
+		list.addLast(50);
+		list.addLast(60);
+		// 10->20->30->40->50->60
+		
+		
+		list.deleteFirst();
+		list.deleteLast();
+		
+		list.display();
+
+	}
+
+}
